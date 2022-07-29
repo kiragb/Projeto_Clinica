c446a82ced95337416dd02de6bb7c33a96de13c3
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-07-29 13:58:03 -0300

    Activity_main_login

---
+++ app/src/main/java/com/example/projetoclinica/PreSessao.kt
@@ -0,0 +1,11 @@
+package com.example.projetoclinica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreSessao : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pre_sessao)
+    }
+}
