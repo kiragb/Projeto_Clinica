9c14ba55dcde3a44fbe3cda780a8448d3d167386
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-07-28 21:16:56 -0300

    Clicar - Botão inicar Sessão

--- app/src/main/java/com/example/projetoclinica/Sessao.kt
+++ app/src/main/java/com/example/projetoclinica/Sessao.kt
@@ -3,6 +3,7 @@ package com.example.projetoclinica
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_sessao.*
 import kotlinx.android.synthetic.main.activity_tela_agenda.*
@@ -10,7 +11,7 @@ import kotlinx.android.synthetic.main.activity_tela_agenda.*
 class Sessao : AppCompatActivity() {
 
     var vermaisinfo: TextView? = null
-
+    var Iniciar_Sessao: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sessao)
@@ -18,6 +19,13 @@ class Sessao : AppCompatActivity() {
         vermaisinfo = findViewById(R.id.vermaisinfo)
         vermaisinfo!!.setOnClickListener {
             val intent = Intent(this@Sessao, Detalhes_da_Sessao::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        Iniciar_Sessao = findViewById(R.id.Iniciar_Sessao)
+        Iniciar_Sessao!!.setOnClickListener {
+            val intent = Intent(this@Sessao, Passo_a_Passo::class.java)
             startActivity(intent)
             finish()
         }
