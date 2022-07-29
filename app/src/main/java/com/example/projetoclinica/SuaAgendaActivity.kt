35e4bd46835bbcc469d20465182adacf174a7d4b
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-07-29 18:21:15 -0300

    ListView sem card

--- app/src/main/java/com/example/projetoclinica/SuaAgendaActivity.kt
+++ app/src/main/java/com/example/projetoclinica/SuaAgendaActivity.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import android.widget.ArrayAdapter
 import android.widget.Toast.makeText
 import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
@@ -12,54 +13,28 @@ import androidx.fragment.app.Fragment
 import java.util.*
 
 
-class SuaAgendaActivity : Fragment(), View.OnClickListener{
+class SuaAgendaActivity : Fragment(){
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
             View? {
         return inflater.inflate(R.layout.sua_agenda, container, false)
     }
-    var DataPosterior: ImageView? = null
-    var DataAnterior: ImageView? = null
-    var DiaeMes: TextView? = null
+
 
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        DataPosterior = view.findViewById<View>(R.id.DataPosterior) as ImageView
-        DataAnterior = view.findViewById<View>(R.id.DataAnterior) as ImageView
+        val Agenda = view.findViewById<ListView>(R.id.Agenda)
 
-        DataPosterior!!.setOnClickListener(this)
+        val nomes= arrayOf("10h", "11h", "12h", "13h", "14h", "15h", "16h", "17h", "18h", "19h", "20h", "21h", "22h")
 
-        view.findViewById<TextView>(R.id.DiaeMes) as TextView
-        DiaeMes?.setOnClickListener{ Toast.makeText(this.activity,
-           "jsssssss", Toast.LENGTH_LONG).show() }
-    }
+        Agenda!!.adapter = ArrayAdapter(
+            view.context,
+            android.R.layout.simple_list_item_1, nomes
+        )
 
-    override fun onClick(p0: View?) {
-        
-    }
-}
-
-/* class agenda: AppCompatActivity() {
-
-    var DataPosterior: ImageView? = null
-    var DataAnterior: ImageView? = null
-    var DiaeMes: TextView? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.sua_agenda)
-
-        DataPosterior = findViewById<View>(R.id.DataPosterior) as ImageView
-        DataAnterior = findViewById<View>(R.id.DataAnterior) as ImageView
-
-        DataPosterior!!.setOnClickListener{
-
-            @Override
-           fun onClick(v: View?)
-            {
-                Toast.makeText(this, "imagem não encotrada",Toast.LENGTH_SHORT).show();
-            }
         }
 }
-    }*/
+
+
 
