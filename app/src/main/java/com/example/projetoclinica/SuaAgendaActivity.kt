143807d09be790b8b5c8d90816a1defb7313d3f1
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-07-31 17:50:17 -0300

    eventos colocados nos cards

--- app/src/main/java/com/example/projetoclinica/SuaAgendaActivity.kt
+++ app/src/main/java/com/example/projetoclinica/SuaAgendaActivity.kt
@@ -7,29 +7,76 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.annotation.Nullable
 import androidx.fragment.app.Fragment
+import android.content.Intent as Intent
 
 
-class SuaAgendaActivity : Fragment(){
+class SuaAgendaActivity : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ):
             View? {
         return inflater.inflate(R.layout.sua_agenda, container, false)
     }
 
+    var BotãoCard1: Button? = null
+    var BotãoCard2: Button? = null
+    var BotãoCard3: Button? = null
+    var BotãoCard4: Button? = null
 
     override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val Agenda = view.findViewById<ListView>(R.id.Agenda)
 
-        val txtTitle= arrayOf("10h", "11h", "12h", "13h", "14h", "15h", "16h", "17h", "18h", "19h", "20h", "21h", "22h")
+        val txtTitle = arrayOf(
+            "10h",
+            "11h",
+            "12h",
+            "13h",
+            "14h",
+            "15h",
+            "16h",
+            "17h",
+            "18h",
+            "19h",
+            "20h",
+            "21h",
+            "22h"
+        )
 
         Agenda!!.adapter = MeuAdapter(
-            view.context, txtTitle )
+            view.context, txtTitle
+        )
 
+        BotãoCard1 = view.findViewById(R.id.BotãoCard1)
+        BotãoCard1!!.setOnClickListener {
+            val intent = Intent(view.context, Sessao::class.java)
+            startActivity(intent)
+        }
 
-    }}
+        BotãoCard2 = view.findViewById(R.id.BotãoCard2)
+        BotãoCard2!!.setOnClickListener {
+            val intent = Intent(view.context, Sessao::class.java)
+            startActivity(intent)
+        }
 
+        BotãoCard3 = view.findViewById(R.id.BotãoCard3)
+        BotãoCard3!!.setOnClickListener {
+            val intent = Intent(view.context, Sessao::class.java)
+            startActivity(intent)
+        }
+
+        BotãoCard4 = view.findViewById(R.id.BotãoCard4)
+        BotãoCard4!!.setOnClickListener {
+            val intent = Intent(view.context, Sessao::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
 
 
 
