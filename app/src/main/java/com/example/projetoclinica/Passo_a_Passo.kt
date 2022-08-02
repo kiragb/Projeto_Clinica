558fdcbd82f7eacf6185ffdf900a1e9552cc0854
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-08-02 18:07:12 -0300

    Clicar no abdomen

--- app/src/main/java/com/example/projetoclinica/Passo_a_Passo.kt
+++ app/src/main/java/com/example/projetoclinica/Passo_a_Passo.kt
@@ -1,11 +1,23 @@
 package com.example.projetoclinica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Passo_a_Passo : AppCompatActivity() {
+
+    var Entrar: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_passo_apasso)
+
+        Entrar = findViewById(R.id.Entrar)
+        Entrar!!.setOnClickListener {
+            val intent = Intent(this@Passo_a_Passo, Pre_Sessao::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/projetoclinica/Pre_Sessao.kt
+++ app/src/main/java/com/example/projetoclinica/Pre_Sessao.kt
@@ -1,11 +1,23 @@
 package com.example.projetoclinica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Pre_Sessao : AppCompatActivity() {
+
+    var abdomen: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pre_sessao)
+
+        abdomen = findViewById(R.id.abdomem)
+        abdomen!!.setOnClickListener {
+            val intent = Intent(this@Pre_Sessao, Abdomen::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
 }
