bd6cff569fc887337d45deb5b001af16be11a6f2
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-08-03 18:51:55 -0300

    Adicionar mais locais costas

--- app/src/main/java/com/example/projetoclinica/Costas.kt
+++ app/src/main/java/com/example/projetoclinica/Costas.kt
@@ -1,11 +1,21 @@
 package com.example.projetoclinica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Costas : AppCompatActivity() {
+    var Adicionar: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_costas)
+
+        Adicionar = findViewById(R.id.Adicionar)
+        Adicionar!!.setOnClickListener {
+            val intent = Intent(this@Costas, Adicionar_Mais_Locais_Costas::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
