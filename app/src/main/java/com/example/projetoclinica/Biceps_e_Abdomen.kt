05856a362666c6e2c2c5a34ba433a00d9b5907c1
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-08-04 13:45:33 -0300

    AST Biceps e Abdomen

--- app/src/main/java/com/example/projetoclinica/Biceps_e_Abdomen.kt
+++ app/src/main/java/com/example/projetoclinica/Biceps_e_Abdomen.kt
@@ -1,11 +1,23 @@
 package com.example.projetoclinica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Biceps_e_Abdomen : AppCompatActivity() {
+
+    var Continuar: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_biceps_e_abdomen)
+
+        Continuar = findViewById(R.id.Continuar)
+        Continuar!!.setOnClickListener {
+            val intent = Intent(this@Biceps_e_Abdomen, AST_Biceps_e_Abdomen::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
