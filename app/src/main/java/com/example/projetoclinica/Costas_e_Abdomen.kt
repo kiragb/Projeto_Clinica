443da223681fd7352fdd4cd1cb4b2d369d9b8dbc
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-08-04 14:04:51 -0300

    AST Coxas e Costas Abdomen

--- app/src/main/java/com/example/projetoclinica/Costas_e_Abdomen.kt
+++ app/src/main/java/com/example/projetoclinica/Costas_e_Abdomen.kt
@@ -1,11 +1,22 @@
 package com.example.projetoclinica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Costas_e_Abdomen : AppCompatActivity() {
+
+    var Continuar: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_costas_e_abdomen)
+
+        Continuar = findViewById(R.id.Continuar)
+        Continuar!!.setOnClickListener {
+            val intent = Intent(this@Costas_e_Abdomen, AST_Costas_e_Abdomen::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/projetoclinica/Coxas_e_Abdomen.kt
+++ app/src/main/java/com/example/projetoclinica/Coxas_e_Abdomen.kt
@@ -1,11 +1,22 @@
 package com.example.projetoclinica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Coxas_e_Abdomen : AppCompatActivity() {
+
+    var Continuar: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coxas_e_abdomen)
+
+        Continuar = findViewById(R.id.Continuar)
+        Continuar!!.setOnClickListener {
+            val intent = Intent(this@Coxas_e_Abdomen, AST_Coxas_e_Abdomen::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
