3ab64279d8361bd9bda80a93e012e0d6474c5470
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-08-04 13:16:08 -0300

    pra aline ver

--- app/src/main/java/com/example/projetoclinica/Abdomen.kt
+++ app/src/main/java/com/example/projetoclinica/Abdomen.kt
@@ -6,15 +6,22 @@ import android.os.Bundle
 import android.widget.Button
 
 class Abdomen : AppCompatActivity() {
-
+    var Continuar: Button? = null
     var Adicionar: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_abdomen)
 
-         Adicionar = findViewById(R.id.Adicionar)
+        Adicionar = findViewById(R.id.Adicionar)
         Adicionar!!.setOnClickListener {
             val intent = Intent(this@Abdomen, Adicionar_Mais_Locais_Abdomen::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        Continuar = findViewById(R.id.Continuar)
+        Continuar!!.setOnClickListener {
+            val intent = Intent(this@Abdomen, AST_Abdomen::class.java)
             startActivity(intent)
             finish()
         }
