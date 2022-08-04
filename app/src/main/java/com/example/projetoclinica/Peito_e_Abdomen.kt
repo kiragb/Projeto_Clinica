963d37851435dd6e1f0ef0058af398105532ebae
Walquíria Gonçalves Barbosa <kirabarbosa05@gmail.com>
2022-08-04 13:55:20 -0300

    AST Peito e Abdomen

--- app/src/main/java/com/example/projetoclinica/Peito_e_Abdomen.kt
+++ app/src/main/java/com/example/projetoclinica/Peito_e_Abdomen.kt
@@ -1,11 +1,22 @@
 package com.example.projetoclinica
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Peito_e_Abdomen : AppCompatActivity() {
+
+    var Continuar: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_peito_e_abdomen)
+
+        Continuar = findViewById(R.id.Continuar)
+        Continuar!!.setOnClickListener {
+            val intent = Intent(this@Peito_e_Abdomen, AST_Peito_e_Abdomen::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
